ff9c852378246d33781622de81f236dcee6656b4
Sagar Waghunde <sagar@Sagars-MacBook-Pro.local>
2025-11-17 07:15:45 +0800

    used with structure instead of PydanticOutputParser

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from langchain.agents import AgentExecutor
 from langchain.agents.react.agent import create_react_agent
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
-from langchain_core.output_parsers.pydantic import PydanticOutputParser
+# from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
 
@@ -17,12 +17,14 @@ load_dotenv()
 
 tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+structured_llm = llm.with_structured_output(AgentResponse)
 react_prompt = hub.pull("hwchase17/react")
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+# output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
 react_prompt_with_format_instructions = PromptTemplate(
     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
     input_variables=["input", "agent_scratchpad", "tool_names"],
-    partial_variables={"format_instructions": output_parser.get_format_instructions()},
+    # partial_variables={"format_instructions": output_parser.get_format_instructions()},
+    partial_variables={"format_instructions": ""}, ## can use empty strings with structured llm for format instructions
 )
 agent = create_react_agent(
     llm=llm, 
@@ -31,8 +33,9 @@ agent = create_react_agent(
 )
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 extract_output = RunnableLambda(lambda x: x.get("output"))
-parse_output = RunnableLambda(lambda x: output_parser.parse(x))
-chain = agent_executor | extract_output | parse_output
+# parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+# chain = agent_executor | extract_output | parse_output
+chain = agent_executor | extract_output | structured_llm
 # @tool
 # def search_tavily(query: str) -> str:
 #     """Search the web for information."""
