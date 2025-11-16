a72327e385814c23c4670937026d31b960853ee4
Sagar Waghunde <sagar@Sagars-MacBook-Pro.local>
2025-11-17 06:57:08 +0800

    Added runnable lambda

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # react-agent-job-search
+
+models : https://langchaincourse.notion.site/1a342623556380c4baa3daba95f6e34f?v=1a3426235563803cbca7000c39ebfa23
+LangChainHub : https://smith.langchain.com/hub?organizationId=d924c6d5-9297-4b9b-9cdb-df9eea6b95a7
+Tavily : https://app.tavily.com/home
--- main.py
+++ main.py
@@ -1,24 +1,43 @@
+import os
 
 from dotenv import load_dotenv
 from langchain import hub
-from langchain.agents.react.agent import create_react_agent
 from langchain.agents import AgentExecutor
-from langchain_tavily import TavilySearch
+from langchain.agents.react.agent import create_react_agent
 from langchain_openai import ChatOpenAI
-import os
+from langchain_tavily import TavilySearch
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
+
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 load_dotenv()
 
 tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 react_prompt = hub.pull("hwchase17/react")
-agent = create_react_agent(llm, tools, prompt=react_prompt)
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"],
+    partial_variables={"format_instructions": output_parser.get_format_instructions()},
+)
+agent = create_react_agent(
+    llm=llm, 
+    tools=tools, 
+    prompt=react_prompt_with_format_instructions
+)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-chain = agent_executor
+extract_output = RunnableLambda(lambda x: x.get("output"))
+parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+chain = agent_executor | extract_output | parse_output
 # @tool
 # def search_tavily(query: str) -> str:
 #     """Search the web for information."""
 #     return TavilySearch().run(query)
+
 
 def main():
     response = chain.invoke(
@@ -27,6 +46,7 @@ def main():
         }
     )
     print(response)
+
 
 if __name__ == "__main__":
     main()
